6da9cce907fe561a280b6f4465c6e79118e1babb
Prince raj <princerajm31@gmail.com>
2025-06-11 11:31:30 +0530

    vite demo

---
+++ 01basic/src/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+
+function App() {
+  return (
+   <h1>Prince Raj</h1>
+  );
+}
+
+export default App;
---
+++ 01vitebasic/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Chai from './chai.jsx'
+
+function App() {
+
+  return (
+    <Chai />
+  )
+  
+}
+
+export default App
